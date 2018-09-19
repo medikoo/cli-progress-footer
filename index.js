0b015a3c21171bc6a51cbc92252dba57bd0e5b0f
Mariusz Nowak <medyk@medikoo.com>
2018-09-19 17:40:00 +0200

    💥

---
+++ index.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const isObject            = require("es5-ext/object/is-object")
+    , count               = require("es5-ext/string/#/count")
+    , ensureString        = require("es5-ext/object/validate-stringifiable-value")
+    , d                   = require("d")
+    , cliErase            = require("cli-color/erase")
+    , cliMove             = require("cli-color/move")
+    , strip               = require("cli-color/strip")
+    , overrideStdoutWrite = require("process-utils/override-stdout-write")
+    , overrideStderrWrite = require("process-utils/override-stderr-write");
+
+const defaultOptions = { overrideStdout: true, redirectStderr: true };
+
+class CliProgressFooter {
+	constructor(options) {
+		Object.defineProperties(this, {
+			_isStdoutOverriden: d(false),
+			_isStderrRedirected: d(false),
+			_progressLinesCount: d(0),
+			_progressOverflowLinesCount: d(0),
+			_lastOutLineLength: d(0)
+		});
+		this.overrideStd(options);
+	}
+	overrideStd(options = defaultOptions) {
+		if (!isObject(options)) options = defaultOptions;
+		if (options.overrideStdout && !this._isStdoutOverriden) {
+			this._stdoutData = overrideStdoutWrite(data => this._repaint(data));
+			this._writeOriginalStdout = this._stdoutData.superStdoutWrite;
+			this._isStdoutOverriden = true;
+		}
+		if (options.redirectStderr && !this._isStderrRedirected) {
+			this._stderrData = overrideStderrWrite(data => process.stdout.write(data));
+			this._isStderrRedirected = true;
+		}
+	}
+	restoreStd() {
+		if (this._isStdoutOverriden) {
+			this._stdoutData.restoreStdoutWrite();
+			delete this._writeOriginalStdout;
+			this._isStdoutOverriden = false;
+		}
+		if (this._isStderrRedirected) {
+			this._stderrData.restoreStderrWrite();
+			this._isStderrRedirected = false;
+		}
+	}
+	updateProgress(data) {
+		this._progressContent = ensureString(data);
+		this._repaint();
+		const newProgressLinesCount = count.call(data, "\n") + 1;
+		if (newProgressLinesCount < this._progressLinesCount) {
+			this._progressOverflowLinesCount += this._progressLinesCount - newProgressLinesCount;
+		}
+		this._progressLinesCount = newProgressLinesCount;
+	}
+	writeStdout(data) { this._repaint(ensureString(data)); }
+
+	_writeOriginalStdout(data) { process.stdout.write(data); }
+	_repaint(data = null) {
+		if (this._progressOverflowLinesCount) {
+			// Go to last line
+			this._writeOriginalStdout(cliMove.bottom);
+		}
+
+		const progressBufferLinesCount =
+			this._progressLinesCount + this._progressOverflowLinesCount;
+		if (progressBufferLinesCount) {
+			// Clear each progress line
+			for (let i = 0; i < progressBufferLinesCount; ++i) {
+				this._writeOriginalStdout(`${ cliErase.line }${ cliMove.lines(-1) }`);
+			}
+			// Position at last out char
+			if (this._lastOutLineLength) {
+				this._writeOriginalStdout(`${ cliMove.right(this._lastOutLineLength) }`);
+			}
+		}
+
+		// Write eventual new std content
+		if (data) {
+			this._writeOriginalStdout(data);
+			if (this._progressOverflowLinesCount) {
+				this._progressOverflowLinesCount = Math.max(
+					this._progressOverflowLinesCount - count.call(data, "\n"), 0
+				);
+			}
+			const lastNewLineIndex = data.lastIndexOf("\n");
+			if (lastNewLineIndex === -1) this._lastOutLineLength += strip(data).length;
+			this._lastOutLineLength = strip(data.slice(lastNewLineIndex + 1)).length;
+		}
+
+		if (!this._progressContent) return;
+
+		// Write progess footer
+		this._writeOriginalStdout(`\n${ this._progressContent }`);
+	}
+}
+
+module.exports = (options = defaultOptions) => new CliProgressFooter(options);
---
+++ test/index.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const repeat                = require("es5-ext/string/#/repeat")
+    , test                  = require("tape")
+    , ansiRegex             = require("ansi-regex")()
+    , cliErase              = require("cli-color/erase")
+    , cliMove               = require("cli-color/move")
+    , initCliProgressFooter = require("../");
+
+const exposeAnsi = str => str.replace(ansiRegex, csi => `[CSI ${ csi.slice(2) }]`);
+
+test("cliProgressFooter", t => {
+	const cliProgressFooter = initCliProgressFooter({
+		overrideStdout: false,
+		redirectStderr: false
+	});
+	let out = "", expectedOut = "";
+	cliProgressFooter._writeOriginalStdout = data => (out += data);
+
+	cliProgressFooter.writeStdout("sample 1\n");
+	expectedOut += "sample 1\n";
+
+	cliProgressFooter.writeStdout("sample 2\n");
+	expectedOut += "sample 2\n";
+
+	cliProgressFooter.updateProgress(
+		`${ [1, 2, 3, 4].map(item => `# item ${ item }`).join("\n") }\n`
+	);
+	expectedOut += `\n${ [1, 2, 3, 4].map(item => `# item ${ item }`).join("\n") }\n`;
+
+	cliProgressFooter.writeStdout("sample 3\n");
+	expectedOut += `${ repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 5) }sample 3\n\n${
+		[1, 2, 3, 4].map(item => `# item ${ item }`).join("\n")
+	}\n`;
+
+	cliProgressFooter.updateProgress(
+		`${ [1, 2, 3, 4, 5].map(item => `# item ${ item }`).join("\n") }\n`
+	);
+	expectedOut += `${ repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 5) }\n${
+		[1, 2, 3, 4, 5].map(item => `# item ${ item }`).join("\n")
+	}\n`;
+
+	cliProgressFooter.writeStdout("sample 4\n");
+	expectedOut += `${ repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 6) }sample 4\n\n${
+		[1, 2, 3, 4, 5].map(item => `# item ${ item }`).join("\n")
+	}\n`;
+
+	cliProgressFooter.updateProgress(`${ [1, 2].map(item => `# item ${ item }`).join("\n") }\n`);
+	expectedOut += `${ repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 6) }\n${
+		[1, 2].map(item => `# item ${ item }`).join("\n")
+	}\n`;
+
+	cliProgressFooter.writeStdout("FOo\n");
+	expectedOut += `${ cliMove.bottom }${
+		repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 6)
+	}FOo\n\n${ [1, 2].map(item => `# item ${ item }`).join("\n") }\n`;
+
+	cliProgressFooter.writeStdout("Mark");
+	expectedOut += `${ cliMove.bottom }${
+		repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 5)
+	}Mark\n${ [1, 2].map(item => `# item ${ item }`).join("\n") }\n`;
+
+	cliProgressFooter.writeStdout("Mark\n");
+	expectedOut += `${ cliMove.bottom }${
+		repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 5)
+	}${ cliMove.right(4) }Mark\n\n${ [1, 2].map(item => `# item ${ item }`).join("\n") }\n`;
+
+	cliProgressFooter.updateProgress(`${ [1, 3].map(item => `# item ${ item }`).join("\n") }\n`);
+	expectedOut += `${ cliMove.bottom }${
+		repeat.call(`${ cliErase.line }${ cliMove.lines(-1) }`, 4)
+	}\n${ [1, 3].map(item => `# item ${ item }`).join("\n") }\n`;
+
+	t.equal(exposeAnsi(out), exposeAnsi(expectedOut));
+	t.end();
+});
